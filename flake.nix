e14d78f59779800edaf5f75207a143f790ea31ed
Nikolai Shields <nikolai@nikolaishields.com>
2022-06-29 15:33:15 -0500

    Utilize flake-utils to build for all system targets

--- flake.nix
+++ flake.nix
@@ -1,49 +1,39 @@
 {
   description = "An interactive environment for working at SUSE";
   inputs.nixpkgs.url = "nixpkgs/nixos-22.05";
-  outputs = { self, nixpkgs }:
-    let
-      lastModifiedDate = self.lastModifiedDate or self.lastModified or "19700101";
-      version = builtins.substring 0 8 lastModifiedDate;
-      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
-      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
-    in
-    {
-      devShells = forAllSystems (system:
-        let 
-          pkgs = nixpkgsFor.${system};
-        in {
-          default = pkgs.mkShell {
-            buildInputs = with pkgs; [ 
-              age
-              bashInteractive
-              bats
-              binutils
-              curl
-              file
-              gcc
-              gh
-              git 
-              gnumake
-              go-tools
-              go_1_18
-              gopls
-              gotools
-              nettools
-              nixpkgs-fmt
-              openssl
-              shellcheck
-              shfmt
-              sops
-              terraform
-              terragrunt
-              wget
-              yq
-            ];
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            age
+            bashInteractive
+            bats
+            binutils
+            curl
+            file
+            gcc
+            gh
+            git
+            gnumake
+            go-tools
+            go_1_18
+            gopls
+            gotools
+            nettools
+            nixpkgs-fmt
+            openssl
+            shellcheck
+            shfmt
+            sops
+            terraform
+            terragrunt
+            wget
+            yq
+          ];
         };
       });
-      devShell = forAllSystems (system: self.devShells.${system}.default);
-    };
 }
-
